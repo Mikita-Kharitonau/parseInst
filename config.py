2b15c04fff0cb11919b0a327573f1e519bfbd4a1
Mikita-Kharitonau <nikita.kharitonov99@gmail.com>
2018-12-26 18:56:58 +0300

    changed 'photoSrc' from post image to location image

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 HTML_PARSER_CONFIG = {
     "location_html_class": "O4GlU",
     "location_url_prefix": "/explore/locations/",
-    "img_html_class": "FFVAD",
+    "post_img_html_class": "FFVAD",
+    "location_img_html_class": "ECCnW",
     "lat_property_value": "place:location:latitude",
     "lon_property_value": "place:location:longitude",
     "phantomjs_exec_path": os.path.join(
--- html_parser/html_parser.py
+++ html_parser/html_parser.py
@@ -5,18 +5,23 @@ from selenium import (
 )
 from flask import (
     jsonify,
-    abort
+    abort,
+    make_response
 )
+from urllib import request
+from bs4 import BeautifulSoup
 
 
 def get_parsed_location(post_url, config):
-    driver = prepare_driver_with_url(post_url, config)
+    try:
+        driver = prepare_driver_with_url(post_url, config)
+    except:
+        return make_response(jsonify({"error": "Can't initialize webdriver"}), 500)
     try:
         location_dom = driver.find_element_by_class_name(config['location_html_class'])
-        img_dom = driver.find_element_by_class_name(config['img_html_class'])
         try:
-            location_details_url = location_dom.get_attribute("href")
-            lat, lon = get_location_details(location_details_url, config)
+            location_details_url = location_dom.get_attribute('href')
+            lat, lon, location_img_url = get_location_details(location_details_url, config)
             return jsonify(
                 {
                     "id": get_location_id(location_details_url, config),
@@ -25,7 +30,7 @@ def get_parsed_location(post_url, config):
                         "lat": lat,
                         "lon": lon,
                     },
-                    "photoSrc": img_dom.get_attribute("src")
+                    "photoSrc": location_img_url
                 }
             )
         except:
@@ -64,7 +69,22 @@ def get_location_details(location_details_url, config):
         "//meta[@property='{lon}']".format(lon=config['lon_property_value'])
     ).get_attribute("content")
 
+    location_img_url = driver.find_element_by_class_name(
+        config['location_img_html_class']).get_attribute("src")
+
     driver.quit()
+    return lat, lon, location_img_url
+
+
+def get_location_details_with_urllib(location_details_url, config):
+    response = request.urlopen(location_details_url)
+    html = response.read()
+    soup = BeautifulSoup(html)
+    lat = soup.find("meta", property=config['lat_property_value']).get('content', 0)
+    lon = soup.find("meta", property=config['lon_property_value']).get('content', 0)
+    # location_img_url = soup.findAll("img", class_=config['location_img_html_class'])[0].get('src', 'No value')
     return lat, lon
+
+
 
 
